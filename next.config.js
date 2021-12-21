644bdb238583b1e493fb00b48fdad9b5c6599f1c
Giorgi Chumburidze <giorgi@esflow.io>
2021-12-21 20:18:20 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+})
+
+module.exports = withNextra({
   reactStrictMode: true,
-}
+})
---
+++ theme.config.js
@@ -0,0 +1,30 @@
+import { BsSlack } from 'react-icons/bs'
+
+export default {
+    projectChatLink: 'https://relocify.slack.com/',
+    projectChatLinkIcon: <BsSlack />,
+    titleSuffix: ' – Redocs',
+    nextLinks: true,
+    prevLinks: true,
+    search: true,
+    customSearch: null, // customizable, you can use algolia for example
+    darkMode: true,
+    footer: true,
+    footerText: `${new Date().getFullYear()} © Esflow Technologies B.V.`,
+    footerEditLink: false,
+    logo: (
+        <b>
+            <span className="mr-2 font-extrabold md:inline">REDOCS</span>
+        </b>
+    ),
+    head: (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="Nextra: the next docs builder" />
+            <meta name="og:title" content="Nextra: the next docs builder" />
+        </>
+    ),
+    unstable_staticImage: true,
+    defaultMenuCollapsed: false,
+    floatTOC: true,
+}
