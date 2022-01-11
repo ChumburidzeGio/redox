bf85a915155177f8d28565cb356d00b5f8c24c74
Giorgi Chumburidze <giorgi@esflow.io>
2022-01-11 12:15:03 +0100

    add badge

--- blocks/card.tsx
+++ blocks/card.tsx
@@ -9,7 +9,7 @@ export const YoutubeCard: React.FC<Props> = ({ videoId, children }) => {
     return (
         <div style={{ display: 'flex', flexFlow: 'row', border: "2px solid #c1c1c1", borderRadius: "8px", marginTop: "20px", overflow: 'hidden' }}>
             <div style={{ display: 'flex', flex: "1 0 auto" }}><Youtube id={videoId} height={200} /></div>
-            <div style={{ display: 'flex', padding: "10px 15px" }}>{children}</div>
+            <div style={{ display: 'flex', flexFlow: 'column', padding: "10px 15px" }}>{children}</div>
         </div>
     )
 }
