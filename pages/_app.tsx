69e0df53ab746afa668e0ebae34df39153380ada
Giorgi Chumburidze <giorgi@esflow.io>
2022-01-13 12:32:36 +0100

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
+import * as React from 'react'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import amplitude from "amplitude-js"
+
 import 'nextra-theme-docs/style.css'
 import '../blocks/services.css'
 
+const initAmplitude = () => {
+  amplitude.getInstance().init("127e72a08a594d038656516bb021f04b", undefined, {
+    includeUtm: true,
+    includeReferrer: true,
+    includeGclid: true,
+    includeFbclid: true
+  });
+};
+
 export default function Nextra({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  React.useEffect(() => {
+    if (router.isReady) {
+      initAmplitude()
+    }
+  }, [router.isReady])
+
+  return (
+        <Component {...pageProps} />
+  )
 }
