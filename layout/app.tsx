70b739813241bd2514076d365dcea1511d72c4cf
Giorgi Chumburidze <giorgi@esflow.io>
2022-02-08 11:11:03 +0100

    remove anchors

--- layout/app.tsx
+++ layout/app.tsx
@@ -9,7 +9,7 @@ const navigation = [
 
 export const AppLayout: React.FC = ({ children }) => {
     return (
-       <BaseLayout sectionAnchors={[]} navigation={navigation}>
+       <BaseLayout navigation={navigation}>
            <div className="flex-1 relative z-0 flex overflow-hidden min-h-screen bg-gray-100">
                <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none mx-auto h-full min-h-screen px-4 md:px-8 lg:px-0 pt-5 md:pt-8 pb-8">
                    {children}
--- layout/docs.tsx
+++ layout/docs.tsx
@@ -38,7 +38,7 @@ export const DocsLayout: React.FC = ({ children }) => {
 
     return (
         <MDXProvider components={components}>
-            <BaseLayout sectionAnchors={sectionAnchors} navigation={navigation}>
+            <BaseLayout navigation={navigation}>
                 <div className="flex-1 relative z-0 flex overflow-hidden">
                     <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none mx-auto max-w-3xl px-4 md:px-8 lg:px-0 pt-5 md:pt-8 pb-8">
                         {children}
