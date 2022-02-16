dacdb28fa0202b500979247b5e5f4f3759e4b3a8
Giorgi Chumburidze <giorgi@esflow.io>
2022-02-16 10:39:22 +0100

    add footer navigation

--- layout/docs.tsx
+++ layout/docs.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 
 import { extractHeaders } from './mdx-components/extractHeaders'
 import {BaseLayout} from "./base";
+import { FooterNavigation } from "./navigation";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 export const DocsLayout: React.FC = ({ children }) => {
@@ -12,6 +13,7 @@ export const DocsLayout: React.FC = ({ children }) => {
             <div className="flex-1 relative z-0 flex overflow-hidden pt-4">
                 <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none mx-auto max-w-3xl px-4 md:px-8 lg:px-0 pt-5 md:pt-8 pb-28">
                     {children}
+                    <FooterNavigation />
                 </main>
                 <aside className="hidden relative xl:flex xl:flex-col flex-shrink-0 w-64 border-r border-gray-200 overflow-y-auto">
                     <ul className="list-none pt-10 fixed">
