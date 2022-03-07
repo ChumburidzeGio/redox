9ec9d6074c51dff9728e297a5670277c1781ec7f
Giorgi Chumburidze <giorgi@esflow.io>
2022-03-07 12:24:49 +0100

    add icons

--- lib/layouts/navigation.tsx
+++ lib/layouts/navigation.tsx
@@ -3,30 +3,30 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import {ChevronDownIcon} from '@heroicons/react/solid'
 import {signOut} from "next-auth/react";
-import {LogoutIcon} from "@heroicons/react/outline";
+import {LogoutIcon, HomeIcon, MapIcon, AcademicCapIcon, UserGroupIcon, CreditCardIcon, FlagIcon, SparklesIcon } from "@heroicons/react/outline";
 import {logEvent} from "lib/analytics";
 
 export const navigation = [
     { name: 'Getting Started', href: '/' },
-    { name: 'Housing', children: [
+    { name: 'Housing', Icon: HomeIcon, children: [
         { name: "Amsterdam", href: "/dox/housing/amsterdam" },
         { name: "Rental Process", href: "/dox/housing/rental-process" },
         { name: "Utilities", href: "/dox/housing/utilities" },
         { name: "Moving Services (HHG)", href: "/dox/housing/moving-services" },
         { name: "Furnishing Apartment", href: "/dox/housing/furnishing" },
     ]},
-    { name: 'Essentials', children: [
+    { name: 'Essentials', Icon: CreditCardIcon, children: [
         { name: "BSN", href: "/dox/essentials/bsn" },
         { name: "Banking", href: "/dox/essentials/banking" },
         { name: "Insurance", href: "/dox/essentials/insurance" },
     ]},
-    { name: 'Transportation', children: [
+    { name: 'Transportation', Icon: MapIcon, children: [
         { name: "Bicycle", href: "/dox/transportation/bicycle" },
         { name: "Public Transportation", href: "/dox/transportation/public-transport" },
         { name: "Driving License", href: "/dox/transportation/driving-license" },
         { name: "Buying Car", href: "/dox/transportation/buying-car" },
     ]},
-    { name: 'Settling In', children: [
+    { name: 'Settling In', Icon: FlagIcon, children: [
         { name: "DigiD", href: "/dox/settling-in/digid" },
         { name: "MyGovernment", href: "/dox/settling-in/my-government" },
         { name: "Healthcare System", href: "/dox/settling-in/healthcare" },
@@ -35,17 +35,17 @@ export const navigation = [
         { name: "Entertainment", href: "/dox/settling-in/entertainment" },
         { name: "Buying Property", href: "/dox/settling-in/buying-property" },
     ]},
-    { name: 'Education', children: [
+    { name: 'Education', Icon: AcademicCapIcon, children: [
         { name: "Introduction", href: "/dox/education/introduction" },
         { name: "Primary Education", href: "/dox/education/primary" },
         { name: "Secondary Education", href: "/dox/education/secondary" },
         { name: "International Schools", href: "/dox/education/international-schools" },
     ]},
-    { name: 'Family & Pets', children: [
+    { name: 'Family & Pets', Icon: UserGroupIcon,  children: [
         { name: "Child Sports", href: "/dox/family_and_pets/child-sports" },
         { name: "Pets", href: "/dox/family_and_pets/pets" },
     ]},
-    { name: 'Integration', children: [
+    { name: 'Integration', Icon: SparklesIcon, children: [
         { name: "Dutch Language", href: "/dox/integration/language" },
         { name: "Dutch Culture", href: "/dox/integration/culture" },
         { name: "Short History", href: "/dox/integration/history" },
@@ -60,13 +60,14 @@ function classNames(...classes: string[]) {
 
 interface DropdownMenuProps {
     name: string
+    Icon?: React.FC<React.ComponentProps<'svg'>>
     items: {
         name: string
         href: string
     }[]
 }
 
-export const DropdownMenu: React.FC<DropdownMenuProps> = ({ name, items }) => {
+export const DropdownMenu: React.FC<DropdownMenuProps> = ({ name, items, Icon }) => {
     const router = useRouter()
     const [folded, setFolded] = React.useState(() => {
         return !items.find(item => item.href === router.asPath)
@@ -77,7 +78,10 @@ export const DropdownMenu: React.FC<DropdownMenuProps> = ({ name, items }) => {
             <span
                 onClick={() => setFolded((isFolded) => !isFolded)}
                 className="group flex items-center px-2 py-2 text-base font-medium text-gray-600 justify-between cursor-pointer hover:bg-gray-50 hover:text-gray-900">
-                {name}
+                <div className="flex">
+                    {Icon && <Icon className="h-5 w-5 mr-3" />}
+                    {name}
+                </div>
                 <ChevronDownIcon className="w-5 h-5 text-gray-600" />
             </span>
             <div className={classNames(
@@ -88,7 +92,7 @@ export const DropdownMenu: React.FC<DropdownMenuProps> = ({ name, items }) => {
                     <Link href={item.href} key={item.name} passHref>
                         <a className={classNames(
                                 router.asPath === item.href
-                                    ? 'bg-slate-200 text-gray-900'
+                                    ? 'bg-slate-200 text-gray-900 font-semibold'
                                     : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
                                 'ml-4 group flex items-center px-2 py-2 text-base rounded-md'
                             )}
@@ -113,21 +117,22 @@ export const Navigation: React.FC = () => {
     return <>
         <nav className="flex flex-1 px-4 pb-4 pt-3 space-y-1 flex-col justify-between h-full">
             <div className="flex flex-col">
-                {navigation.map((item) => item.children ? (
-                    <DropdownMenu items={item.children} name={item.name} key={item.name} />
+                {navigation.map(({ children, name, href, Icon }) => children ? (
+                    <DropdownMenu items={children} name={name} Icon={Icon} key={name} />
                 ) : (
-                    <Link key={item.name} href={item.href} passHref>
+                    <Link key={name} href={href as string} passHref>
                         <a
-                            key={item.name}
-                            href={item.href}
+                            key={name}
+                            href={href}
                             className={classNames(
-                                router.asPath === item.href
-                                    ? 'bg-slate-200 text-gray-900'
+                                router.asPath === href
+                                    ? 'bg-slate-200 text-gray-900 font-semibold'
                                     : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
                                 'group flex items-center px-2 py-2 text-base font-medium rounded-md'
                             )}
                         >
-                            {item.name}
+                            {Icon && <Icon className="h-4 w-4 mr-2" />}
+                            {name}
                         </a>
                     </Link>
                 ))}
