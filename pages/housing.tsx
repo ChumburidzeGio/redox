6b55d460a52674050e33933a980f8629fd33c0f7
MacBook Air M1 <macbookairm1@MACs-MacBook-Air.local>
2022-03-08 01:58:10 +0400

    added draft icon

--- pages/housing.tsx
+++ pages/housing.tsx
@@ -2,12 +2,17 @@ import * as React from "react";
 
 import { BaseLayout } from "lib/layouts";
 import { Transition } from "@headlessui/react";
+import { HeartIcon } from "@heroicons/react/solid";
+import { HeartIcon as Heart } from "@heroicons/react/outline";
 
 interface mockInterface {
   name: string;
   img: string;
   desc: string;
   price: string;
+  selected: Array<number>;
+  id: number;
+  setSelected: React.Dispatch<React.SetStateAction<number[]>>;
 }
 
 const mockData: Array<mockInterface> = Array(10).fill({
@@ -17,18 +22,47 @@ const mockData: Array<mockInterface> = Array(10).fill({
   price: "€ 1,695 p/mo.",
 });
 
-const Item: React.FC<mockInterface> = ({ price, name, img, desc }) => {
+const Item: React.FC<mockInterface> = ({
+  price,
+  setSelected,
+  selected,
+  name,
+  img,
+  desc,
+  id,
+}) => {
   return (
     <div className="cursor-pointer mt-3 flex w-full gap-10 border-b pb-5 border-gray-500">
       <img src={img} alt={desc} className="h-36" />
-      <div className="flex flex-col gap-2">
-        <h3 className="hover:text-slate-700 text-base font-semibold text-slate-400">
-          {name}
-        </h3>
-        <h4 className="hover:text-gray-900  text-sm font-normal text-black-500">
-          {desc}
-        </h4>
-        <p className="m-0 text-sm font-bold text-black-900">{price}</p>
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col gap-2">
+          <h3 className="hover:text-slate-700 text-base font-semibold text-slate-400">
+            {name}
+          </h3>
+          <h4 className="hover:text-gray-900  text-sm font-normal text-black-500">
+            {desc}
+          </h4>
+          <p className="m-0 text-sm font-bold text-black-900">{price}</p>
+        </div>
+        <div>
+          {selected.includes(id) ? (
+            <HeartIcon
+              className={`h-6 w-6 ml-2 cursor-pointer`}
+              onClick={() =>
+                setSelected((prevState: Array<number>) =>
+                  prevState.filter((idx: number) => idx !== id)
+                )
+              }
+            />
+          ) : (
+            <Heart
+              className="h-6 w-6 ml-2 cursor-pointer"
+              onClick={() =>
+                setSelected((prevState: Array<number>) => [...prevState, id])
+              }
+            />
+          )}
+        </div>
       </div>
     </div>
   );
@@ -36,6 +70,7 @@ const Item: React.FC<mockInterface> = ({ price, name, img, desc }) => {
 
 const Wrapper: React.FC = () => {
   const [show, setShow] = React.useState<boolean>(false);
+  const [selected, setSelected] = React.useState<Array<number>>([]);
 
   React.useEffect(() => {
     setTimeout(() => {
@@ -60,8 +95,11 @@ const Wrapper: React.FC = () => {
             {mockData.map(({ name, desc, img, price }, index: number) => (
               <Item
                 key={index}
+                id={index}
                 price={price}
+                selected={selected}
                 name={name}
+                setSelected={setSelected}
                 desc={desc}
                 img={img}
               />
