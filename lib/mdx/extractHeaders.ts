c863d8f063821c11954cec5e8a9b04c87cf28371
Giorgi Chumburidze <giorgi@esflow.io>
2022-03-16 12:24:48 +0100

    upgrade mdx to v2

--- lib/mdx/extractHeaders.ts
+++ lib/mdx/extractHeaders.ts
@@ -1,21 +1,25 @@
 import * as React from 'react'
 import slugger from "./slugger";
 
-export function extractHeaders(children: React.ReactElement[]) {
+export function extractHeaders(children: React.ReactElement) {
     const headings: {
         name: string
         depth: number
         link: string
     }[] = []
 
-    children.map((child: React.ReactElement) => {
-        const type = child.props?.mdxType
+    if (typeof children.type !== 'function') {
+        return headings
+    }
 
-        if (!type || !['h2', 'h3'].includes(type)) {
-            return
+    children.type().props.children.map((child: React.ReactElement) => {
+        const type = child?.type?.name
+
+        if (!['CustomH2', 'CustomH3'].includes(type)) {
+            return;
         }
 
-        const depth = parseInt(type.replace('h', ''))
+        const depth = parseInt(type.replace('CustomH', ''))
         const name = child.props?.children
         const link = `#${slugger(name)}`
 
