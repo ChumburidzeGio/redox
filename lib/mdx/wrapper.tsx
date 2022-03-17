e76dc825ad846cb4bfd25192f9c2faedd51bb9d4
MacBook Air M1 <macbookairm1@MACs-MacBook-Air.local>
2022-03-17 10:40:31 +0400

    added wrapper type

--- lib/mdx/wrapper.tsx
+++ lib/mdx/wrapper.tsx
@@ -1,30 +1,43 @@
-import * as React from 'react'
+import * as React from "react";
 
-import { extractHeaders } from './extractHeaders'
+import { extractHeaders } from "./extractHeaders";
 import { BaseLayout } from "lib/layouts";
 import { FooterNavigation } from "./footer-navigation";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { JSXElementConstructor } from "react";
 
 export const Wrapper: React.FC = ({ children }) => {
-    const sectionAnchors = React.useMemo(() => extractHeaders(children as React.ReactElement[]), [children])
+  const sectionAnchors = React.useMemo(
+    () =>
+      extractHeaders(
+        children as React.ReactElement<string | JSXElementConstructor<any>>
+      ),
+    [children]
+  );
 
-    return (
-        <BaseLayout>
-            <div className="flex-1 relative z-0 flex overflow-hidden pt-4">
-                <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none mx-auto max-w-3xl px-4 md:px-8 lg:px-0 pt-5 md:pt-8 pb-28">
-                    {children}
-                    <FooterNavigation />
-                </main>
-                <aside className="hidden relative xl:flex xl:flex-col flex-shrink-0 w-64 border-r border-gray-200 overflow-y-auto">
-                    <ul className="list-none pt-10 fixed">
-                        {sectionAnchors.map(anchor => (
-                            <AnchorLink offset="20" key={anchor.link} href={anchor.link}>
-                                <li className={`text-sm mb-2 text-gray-500 hover:text-blue-600 ${anchor.depth === 3 ? 'ml-4' : ''}`}>{anchor.name}</li>
-                            </AnchorLink>
-                        ))}
-                    </ul>
-                </aside>
-            </div>
-        </BaseLayout>
-    )
-}
+  return (
+    <BaseLayout>
+      <div className="flex-1 relative z-0 flex overflow-hidden pt-4">
+        <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none mx-auto max-w-3xl px-4 md:px-8 lg:px-0 pt-5 md:pt-8 pb-28">
+          {children}
+          <FooterNavigation />
+        </main>
+        <aside className="hidden relative xl:flex xl:flex-col flex-shrink-0 w-64 border-r border-gray-200 overflow-y-auto">
+          <ul className="list-none pt-10 fixed">
+            {sectionAnchors.map((anchor) => (
+              <AnchorLink offset="20" key={anchor.link} href={anchor.link}>
+                <li
+                  className={`text-sm mb-2 text-gray-500 hover:text-blue-600 ${
+                    anchor.depth === 3 ? "ml-4" : ""
+                  }`}
+                >
+                  {anchor.name}
+                </li>
+              </AnchorLink>
+            ))}
+          </ul>
+        </aside>
+      </div>
+    </BaseLayout>
+  );
+};
