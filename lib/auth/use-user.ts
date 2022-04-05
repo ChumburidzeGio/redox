817f56203ddc9347d4d9697b98487d9b7f0cdafa
Giorgi Chumburidze <giorgi@esflow.io>
2022-04-05 20:30:35 +0200

    create user hook

---
+++ lib/auth/use-user.ts
@@ -0,0 +1,13 @@
+import { useSession } from "next-auth/react";
+
+export function useUser() {
+    const { data: session, status } = useSession()
+
+    return {
+        isAuth: status === 'authenticated',
+        isLoading: status === 'loading',
+        name: session?.user?.name || undefined,
+        email: session?.user?.email || undefined,
+        role: session?.role,
+    }
+}
