36ee399afe1321a9744a24cda19c37a13cd8f77d
Giorgi Chumburidze <giorgi@esflow.io>
2022-04-17 22:54:34 +0200

    update next and rever middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,9 +1,7 @@
 import { withAuth } from "next-auth/middleware"
 
-export function middleware(req: any) {
-    return withAuth(req, {
-        pages: {
-            signIn: '/auth/signin',
-        },
-    });
-}
+export default withAuth({
+    pages: {
+        signIn: '/auth/signin',
+    },
+})
