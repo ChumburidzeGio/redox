ba5f4069df40620385ddcd93b90deb90b84afe03
Giorgi Chumburidze <giorgi@esflow.io>
2022-04-17 22:44:00 +0200

    add signin page

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,6 +3,9 @@ import { withAuth } from "next-auth/middleware"
 export function middleware(req: any) {
     console.log(process.env.NEXTAUTH_URL, process.env.NEXTAUTH_SECRET);
     return withAuth(req, {
+        pages: {
+            signIn: '/auth/signin',
+        },
         callbacks: {
             authorized: ({ token }) => !!token,
         },
