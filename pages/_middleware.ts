50959975aae3cdb88fbb28a1bd4b98099af98b3a
Giorgi Chumburidze <giorgi@esflow.io>
2022-04-17 22:14:55 +0200

    upgrade next-auth and add logs

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,8 +1,10 @@
 import { withAuth } from "next-auth/middleware"
 
+console.log('_ NEXTAUTH_SECRET', process.env.NEXTAUTH_SECRET)
+
 export default withAuth({
     pages: {
         signIn: '/auth/signin',
         error: '/auth/error',
-    }
+    },
 })
