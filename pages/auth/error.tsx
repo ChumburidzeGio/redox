0b1ce65bd2c5ca323cdd092b0d26f6214bea5fb8
Giorgi Chumburidze <giorgi@esflow.io>
2022-04-17 20:57:28 +0200

    define secret

--- pages/auth/error.tsx
+++ pages/auth/error.tsx
@@ -1,7 +1,7 @@
 export default function Error() {
     return (
         <div className="">
-            {process.env.NEXT_PUBLIC_VERCEL_URL}
+            {process.env.NEXTAUTH_SECRET}
         </div>
     )
 }
