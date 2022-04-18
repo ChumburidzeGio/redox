26c43a1d895cf8713a712508726da496629dad45
Giorgi Chumburidze <giorgi@esflow.io>
2022-04-18 12:07:53 +0200

    update cookies banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,10 +40,10 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
           location="bottom"
           buttonText="Accept Cookies"
           cookieName="redox_cookie_consent"
-          containerClasses="max-w-[400px] rounded-md ml-5 mb-5"
-          buttonWrapperClasses="-mt-2 block w-full mb-3 mx-4"
-          buttonClasses="w-full text-sm font-medium rounded-md w-full py-2 items-center justify-center bg-blue-100 text-blue-700"
-          disableButtonStyles={true}
+          containerClasses="max-w-[400px] sm:rounded-md sm:mx-5 sm:mb-5 left-0 bottom-0 fixed bg-white p-4 shadow-md border-t sm:border border-slate-300"
+          buttonWrapperClasses="mt-3 block w-full"
+          buttonClasses="w-full text-sm font-medium rounded-md w-full py-2 items-center justify-center bg-amber-200 text-amber-900"
+          disableStyles={true}
           expires={365}
       >
         This website uses cookies to enhance the user experience.
