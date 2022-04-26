82994d5be24d6579ff3440c62081d95bead7ee2f
Giorgi Chumburidze <giorgi@esflow.io>
2022-04-26 13:38:17 +0200

    add to auth middleware role based auth rules

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,4 +4,25 @@ export default withAuth({
     pages: {
         signIn: '/auth/signin',
     },
+    callbacks: {
+        authorized: ({ token, req }) => {
+            const path = req.nextUrl.pathname
+            const isGuest = !Boolean(token?.role)
+            const isEmployer = token?.role === 'employer'
+            const isCustomer = token?.role === 'customer'
+
+            if (isCustomer && path.startsWith('/employer/')) {
+                return false
+            }
+
+            if (isEmployer && path.startsWith('/dox/')) {
+                return false
+            }
+
+            return !(isGuest && (
+                path === '/' ||
+                path === '/settings'
+            ));
+        }
+    }
 })
