20b7762ead11725b3e7f2217bd212ecff669095a
Giorgi Chumburidze <giorgi@esflow.io>
2022-05-23 18:43:04 +0200

    extend middleware for better access control

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,29 @@
-import { withAuth } from "next-auth/middleware"
+import { withAuth } from 'next-auth/middleware'
 
 export default withAuth({
-    pages: {
-        signIn: '/auth/signin',
+  pages: {
+    signIn: '/auth/signin',
+  },
+  callbacks: {
+    authorized: ({ token, req }) => {
+      const path = req.nextUrl.pathname
+      const isGuest = !Boolean(token?.role)
+      const isEmployer = token?.role === 'employer'
+      const isCustomer = token?.role === 'customer'
+
+      if (!isGuest && path === '/auth/signup') {
+        return false
+      }
+
+      if (isCustomer && path.startsWith('/employer/')) {
+        return false
+      }
+
+      if (isEmployer && path.startsWith('/dox/')) {
+        return false
+      }
+
+      return !(isGuest && (path === '/' || path === '/settings'))
     },
+  },
 })
