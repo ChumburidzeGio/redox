62ae97e96b17feea8d88c3d3d76a8054d2f6aacf
Giorgi Chumburidze <giorgi@esflow.io>
2022-05-24 17:13:58 +0200

    disable redirects

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,26 +4,26 @@ export default withAuth({
     pages: {
         signIn: '/auth/signin',
     },
-    callbacks: {
-        authorized: ({ token, req }) => {
-            const path = req.nextUrl.pathname
-            const isGuest = !Boolean(token?.role)
-            const isEmployer = token?.role === 'employer'
-            const isCustomer = token?.role === 'customer'
-
-            if (path === '/auth/signup') {
-                return isGuest
-            }
-
-            if (isCustomer && path.startsWith('/employer/')) {
-                return false
-            }
-
-            if (isEmployer && path.startsWith('/dox/')) {
-                return false
-            }
-
-            return !(isGuest && (path === '/' || path === '/settings'))
-        },
-    },
+    // callbacks: {
+    //     authorized: ({ token, req }) => {
+    //         const path = req.nextUrl.pathname
+    //         const isGuest = !Boolean(token?.role)
+    //         const isEmployer = token?.role === 'employer'
+    //         const isCustomer = token?.role === 'customer'
+    //
+    //         if (path === '/auth/signup') {
+    //             return isGuest
+    //         }
+    //
+    //         if (isCustomer && path.startsWith('/employer/')) {
+    //             return false
+    //         }
+    //
+    //         if (isEmployer && path.startsWith('/dox/')) {
+    //             return false
+    //         }
+    //
+    //         return !(isGuest && (path === '/' || path === '/settings'))
+    //     },
+    // },
 })
