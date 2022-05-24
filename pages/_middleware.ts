97788ceef69a18bb5df840e7b6692c6cc045af9d
Giorgi Chumburidze <giorgi@esflow.io>
2022-05-24 10:27:51 +0200

    redirect to login page if its a public route

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,29 +1,29 @@
 import { withAuth } from 'next-auth/middleware'
 
 export default withAuth({
-  pages: {
-    signIn: '/auth/signin',
-  },
-  callbacks: {
-    authorized: ({ token, req }) => {
-      const path = req.nextUrl.pathname
-      const isGuest = !Boolean(token?.role)
-      const isEmployer = token?.role === 'employer'
-      const isCustomer = token?.role === 'customer'
-
-      if (!isGuest && path === '/auth/signup') {
-        return false
-      }
-
-      if (isCustomer && path.startsWith('/employer/')) {
-        return false
-      }
-
-      if (isEmployer && path.startsWith('/dox/')) {
-        return false
-      }
-
-      return !(isGuest && (path === '/' || path === '/settings'))
+    pages: {
+        signIn: '/auth/signin',
     },
-  },
+    callbacks: {
+        authorized: ({ token, req }) => {
+            const path = req.nextUrl.pathname
+            const isGuest = !Boolean(token?.role)
+            const isEmployer = token?.role === 'employer'
+            const isCustomer = token?.role === 'customer'
+
+            if (path === '/auth/signup') {
+                return isGuest
+            }
+
+            if (isCustomer && path.startsWith('/employer/')) {
+                return false
+            }
+
+            if (isEmployer && path.startsWith('/dox/')) {
+                return false
+            }
+
+            return !(isGuest && (path === '/' || path === '/settings'))
+        },
+    },
 })
