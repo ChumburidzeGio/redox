0fcda7d4fe0980bc310bf8e8f1d1df53e5a28f43
Giorgi Chumburidze <giorgi@esflow.io>
2022-06-03 09:08:05 +0200

    replace spinner with skelton

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import EmployeeHome from './dox/home.mdx'
 import { AppLayout } from 'lib/layouts'
 import { EmployerDashboard } from './employers/dashboard'
 import { useUser } from 'lib/auth'
-import { Delay, Spinner } from '../lib/shared-ui'
+import { Delay } from '../lib/shared-ui'
 
 export default function Dashboard() {
     const { role } = useUser()
@@ -18,9 +18,16 @@ export default function Dashboard() {
 
     return (
         <AppLayout>
-            <Delay by={200}>
-                <div className="h-full items-center flex justify-center">
-                    <Spinner size={12} />
+            <Delay by={300}>
+                <div className="animate-pulse flex space-x-4">
+                    <div className="flex-1 space-y-6 py-5">
+                        <div className="h-10 bg-slate-200 rounded w-1/2" />
+                        <div className="space-y-4">
+                            <div className="h-8 bg-slate-200 rounded" />
+                            <div className="h-8 bg-slate-200 rounded" />
+                            <div className="h-8 bg-slate-200 rounded" />
+                        </div>
+                    </div>
                 </div>
             </Delay>
         </AppLayout>
