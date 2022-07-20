cac69f61c4893ad874cb323830774f1b14ef4350
Giorgi Chumburidze <giorgi@esflow.io>
2022-07-20 21:21:40 +0200

    remove redundant files

--- api-lib/handle-error.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosError } from 'axios'
-
-export function handleAxiosError(endpoint: string, error: unknown) {
-    const axiosError = error as AxiosError
-    let message = axiosError.message || ''
-
-    console.log(endpoint, message)
-}
